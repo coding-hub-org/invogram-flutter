70cc0519e8160b59fb331d84d0e5f1aef50906ea
Quan Vo <votrungquan99@gmail.com>
2020-03-21 00:04:33 -0400

    make login form statelesswidget

--- lib/screens/login/login_form.dart
+++ lib/screens/login/login_form.dart
@@ -1,104 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:invogram/providers/authentication_state.dart';
-import 'package:invogram/screens/routes.dart';
+import 'package:invogram/screens/login/input_fields.dart';
 
-class LoginForm extends StatefulWidget {
-  @override
-  _LoginFormState createState() => _LoginFormState();
-}
+class LoginForm extends StatelessWidget {
+  final String error;
+  final bool showPassword;
+  final bool loading;
+  final GlobalKey<FormState> formKey;
+  final Function(String) onChangePassword, onChangeEmail;
+  final Function() onToggleShowPassword;
+  final Function() onClickSignInButton;
 
-class _LoginFormState extends State<LoginForm> {
-  String _email, _password, _error = '';
-  bool _showPassword = true;
-  bool _loading = false;
-
-  final _formKey = GlobalKey<FormState>();
-
-  onChangePassword(String value) {
-    setState(() {
-      _password = value.trim();
-    });
-  }
-
-  onChangeEmail(String value) {
-    setState(() {
-      _email = value.trim();
-    });
-  }
-
-  onClickShowPassword() {
-    setState(() {
-      _showPassword = !_showPassword;
-    });
-  }
-
-  onClickSignInButton() async {
-    setState(() {
-      _loading = true;
-    });
-    if (_formKey.currentState.validate()) {
-      Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text('Logging In'),
-        duration: Duration(minutes: 5),
-      ));
-      String userId = await AuthenticateState().signIn(_email, _password);
-      Scaffold.of(context).hideCurrentSnackBar();
-      if (userId == null) {
-        setState(() {
-          _error = 'Wrong user or password';
-        });
-      } else {
-        setState(() {
-          _error = '';
-        });
-        Navigator.pushNamed(context, Routes.reminder.name);
-      }
-    }
-    setState(() {
-      _loading = false;
-    });
-  }
-
-  Widget emailField() {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(20, 100, 20, 10),
-      child: TextFormField(
-        decoration: InputDecoration(labelText: 'Email'),
-        validator: (value) {
-          bool isValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
-          if (isValid) {
-            return value.isEmpty ? 'Email can\'t be empty' : null;
-          } else {
-            return 'Email is not valid';
-          }
-        },
-        onChanged: (value) => onChangeEmail(value),
-        enabled: !_loading,
-      ),
-    );
-  }
-
-  Widget passwordField() {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-      child: TextFormField(
-        decoration: InputDecoration(
-            labelText: 'Password',
-            suffixIcon: IconButton(
-              icon: Icon(!_showPassword ? Icons.visibility : Icons.visibility_off),
-              onPressed: () {
-                setState(() {
-                  _showPassword = !_showPassword;
-                });
-              },
-            )),
-        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
-        onChanged: (value) => onChangePassword(value),
-        obscureText: _showPassword,
-        enabled: !_loading,
-      ),
-    );
-  }
+  LoginForm({
+    @required this.error,
+    @required this.showPassword,
+    @required this.loading,
+    @required this.formKey,
+    this.onChangePassword,
+    this.onChangeEmail,
+    this.onToggleShowPassword,
+    this.onClickSignInButton,
+  });
 
   Widget signInButton() {
     return Align(
@@ -110,7 +31,9 @@ class _LoginFormState extends State<LoginForm> {
           child: RaisedButton(
             elevation: 10,
             color: Colors.blue,
-            onPressed: () => onClickSignInButton(),
+            onPressed: () {
+              onClickSignInButton();
+            },
             child: Text(
               'Sign In',
               style: TextStyle(fontSize: 30, color: Colors.white),
@@ -163,7 +86,7 @@ class _LoginFormState extends State<LoginForm> {
 
   Widget errorMessage() {
     return Text(
-      _error,
+      error == null ? '' : error,
       style: TextStyle(color: Colors.red),
     );
   }
@@ -171,8 +94,16 @@ class _LoginFormState extends State<LoginForm> {
   Widget signInFields() {
     return Column(
       children: <Widget>[
-        emailField(),
-        passwordField(),
+        EmailField(
+          loading: loading,
+          onChangeEmail: onChangeEmail,
+        ),
+        PasswordField(
+          loading: loading,
+          onChangePassword: onChangePassword,
+          onToggleShowPassword: onToggleShowPassword,
+          showPassword: showPassword,
+        ),
         errorMessage(),
         signInButton(),
       ],
@@ -192,7 +123,7 @@ class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
+      key: formKey,
       child: ListView(
         children: <Widget>[
           Column(
--- lib/screens/login/login_layout.dart
+++ lib/screens/login/login_layout.dart
@@ -3,22 +3,38 @@ import 'package:invogram/screens/login/login_form.dart';
 
 /// TODO: implement this
 class LoginLayout extends StatelessWidget {
-  const LoginLayout({Key key}) : super(key: key);
+  final String error;
+  final bool showPassword;
+  final bool loading;
+  final GlobalKey<FormState> formKey;
+  final Function(String) onChangePassword, onChangeEmail;
+  final Function() onToggleShowPassword;
+  final Function() onClickSignInButton;
+
+  LoginLayout({
+    @required this.error,
+    @required this.showPassword,
+    @required this.loading,
+    @required this.formKey,
+    this.onChangePassword,
+    this.onChangeEmail,
+    this.onToggleShowPassword,
+    this.onClickSignInButton,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Container(
-          child: Text(
-            'Welcome to Invogram',
-          ),
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: LoginForm(),
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: LoginForm(
+        error: error,
+        formKey: formKey,
+        loading: loading,
+        showPassword: showPassword,
+        onChangeEmail: onChangeEmail,
+        onChangePassword: onChangeEmail,
+        onClickSignInButton: onClickSignInButton,
+        onToggleShowPassword: onToggleShowPassword,
       ),
     );
   }
